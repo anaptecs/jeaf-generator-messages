1ad64ca0ea1ca2fab5aa86800d191c884afabdbc
JEAF Development Team <tls@anaptecs.de>
2023-04-14 16:35:54 +0200

    Fixed spelling bugs in new messages

--- jeaf-generator-messages/src-gen/main/java/com/anaptecs/jeaf/generator/ErrorCodes.java
+++ jeaf-generator-messages/src-gen/main/java/com/anaptecs/jeaf/generator/ErrorCodes.java
@@ -459,8 +459,8 @@ public final class ErrorCodes {
   public static final ErrorCode NO_PUBLIC_FIELD_NAME_FOR_COMPOSITE_DATA_TYPE;
 
   /**
-   * In case that a class has one or more bidirectional associations ist not possible to generate equals() and
-   * hashCode() as it requires some business knowledege to do so. If equals() and hashCode() are required for such a
+   * In case that a class has one or more bidirectional associations it is not possible to generate equals() and
+   * hashCode() as it requires some business knowledge to do so. If equals() and hashCode() are required for such a
    * class then it has to be implemented manually.
    */
   public static final ErrorCode NO_EQUALS_FOR_BIDIRECTIONAL_ASSOCIATIONS;
